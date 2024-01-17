860da7cbe777a021d457d770f43552e4b75721b6
Suyash Pramod Gaikwad <153586327+SuyashGaikwad2131@users.noreply.github.com>
2024-01-17 14:06:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,181 +1,195 @@
-//Header fixed
+document.addEventListener("DOMContentLoaded", function () {
+  // Header fixed
+  window.onscroll = function () {
+    const docScrollTop = document.documentElement.scrollTop;
 
-window.onscroll = function () {
-  const docScrollTop = document.documentElement.scrollTop;
-
-  if (window.innerWidth > 991) {
-    if (docScrollTop > 100) {
-      document.querySelector("header").classList.add("fixed");
-    } else {
-      document.querySelector("header").classList.remove("fixed");
+    if (window.innerWidth > 991) {
+      if (docScrollTop > 100) {
+        document.querySelector("header").classList.add("fixed");
+      } else {
+        document.querySelector("header").classList.remove("fixed");
+      }
     }
-  }
-}
+  };
 
-//navbar links
+  // Navbar links
+  const navbar = document.querySelector(".navbar");
+  const links = navbar.querySelectorAll("a");
 
-const navbar = document.querySelector(".navbar");
-a = navbar.querySelectorAll("a")
+  links.forEach(function (link) {
+    link.addEventListener("click", function () {
+      links.forEach(function (otherLink) {
+        otherLink.classList.remove("active");
+      });
 
-a.forEach(function (element) {
-  element.addEventListener("click", function () {
-    for (let i = 0; i < a.length; i++) {
-      a[i].classList.remove("active")
-    }
-    this.classList.add("active");
+      this.classList.add("active");
+      document.querySelector(".navbar").classList.toggle("show");
+    });
+  });
+
+  // Hamburger
+  const hamburger = document.querySelector(".hamburger");
+
+  hamburger.addEventListener("click", function () {
     document.querySelector(".navbar").classList.toggle("show");
-  })
-})
+  });
 
-//Hamburger
+  // Portfolio Gallery
+  const filterBtn = document.querySelector("#filterBtn").children;
+  const items = document.querySelector(".gallery").children;
 
-const hamBurger = document.querySelector(".hamburger");
-
-hamBurger.addEventListener("click", function () {
-  document.querySelector(".navbar").classList.toggle("show");
-})
-
-//Portfolio Gallery
-
-const filterBtn = document.querySelector("#filterBtn").children;
-const item = document.querySelector(".gallery").children;
-
-for (let i = 0; i < filterBtn.length; i++) {
-  filterBtn[i].addEventListener("click", function () {
-    for (let j = 0; j < filterBtn.length; j++) {
-      filterBtn[j].classList.remove("active");
-    }
-    this.classList.add("active");
-    const target = this.getAttribute("data-target");
-    for (let k = 0; k < item.length; k++) {
-      item[k].style.display = "none";
-      if (target == item[k].getAttribute("data-id")) {
-        item[k].style.display = "block";
+  for (let i = 0; i < filterBtn.length; i++) {
+    filterBtn[i].addEventListener("click", function () {
+      for (let j = 0; j < filterBtn.length; j++) {
+        filterBtn[j].classList.remove("active");
       }
-      if (target == "all") {
-        item[k].style.display = "block";
+      this.classList.add("active");
+      const target = this.getAttribute("data-target");
+      for (let k = 0; k < items.length; k++) {
+        items[k].style.display = "none";
+        if (target == items[k].getAttribute("data-id") || target == "all") {
+          items[k].style.display = "block";
+        }
       }
+    });
+  }
+
+  const closeLightbox = document.querySelector(".close-lightbox");
+  const lightbox = document.querySelector(".lightbox");
+  const lightboxImg = lightbox.querySelector("img");
+
+  lightbox.addEventListener("click", function (event) {
+    if (event.target != lightboxImg) {
+      lightbox.classList.remove("show");
+      lightbox.classList.add("hide");
     }
   });
-}
 
-const closeLightbox = document.querySelector(".close-lightbox");
-const lightbox = document.querySelector(".lightbox");
-const lightboxImg = lightbox.querySelector("img");
-
-lightbox.addEventListener("click", function () {
-  if (event.target != lightboxImg) {
+  closeLightbox.addEventListener("click", function () {
     lightbox.classList.remove("show");
     lightbox.classList.add("hide");
-  }
-});
-
-closeLightbox.addEventListener("click", function () {
-  lightbox.classList.remove("show");
-  lightbox.classList.add("hide");
-});
-
-const gallery = document.querySelector(".gallery");
-
-const galleryItem = document.querySelectorAll(".item");
-
-galleryItem.forEach(function (element) {
-  element.querySelector(".fa-plus").addEventListener("click", function () {
-    lightbox.classList.remove("hide");
-    lightbox.classList.add("show");
-    lightboxImg.src = element.querySelector("img").getAttribute("src");
   });
+
+  const gallery = document.querySelector(".gallery");
+  const galleryItems = document.querySelectorAll(".item");
+
+  galleryItems.forEach(function (item) {
+    item.querySelector(".fa-plus").addEventListener("click", function () {
+      lightbox.classList.remove("hide");
+      lightbox.classList.add("show");
+      lightboxImg.src = item.querySelector("img").getAttribute("src");
+    });
+  });
+
+  // Testimonials Slider
+  const sliderContainer = document.querySelector(".testimonials-box");
+  const slider = sliderContainer.children;
+  const containerWidth = sliderContainer.offsetWidth;
+
+  const margin = 30;
+  let itemsPerSlide = 0;
+  let sliderDot;
+
+  const responsive = [
+    { breakPoint: { width: 0, items: 1 } },
+    { breakPoint: { width: 991, items: 2 } },
+  ];
+
+  function load() {
+    for (let i = 0; i < responsive.length; i++) {
+      if (window.innerWidth > responsive[i].breakPoint.width) {
+        itemsPerSlide = responsive[i].breakPoint.items;
+      }
+    }
+    start();
+  }
+
+  function start() {
+    let totalWidth = 0;
+    for (let i = 0; i < slider.length; i++) {
+      slider[i].style.width = containerWidth / itemsPerSlide - margin + "px";
+      slider[i].style.margin = margin / 2 + "px";
+      totalWidth += containerWidth / itemsPerSlide;
+    }
+    sliderContainer.style.width = totalWidth + "px";
+
+    sliderDot = Math.ceil(slider.length / itemsPerSlide);
+
+    for (let i = 0; i < sliderDot; i++) {
+      const div = document.createElement("div");
+      div.id = i;
+      div.setAttribute("onclick", "controlSlide(this)");
+      if (i == 0) {
+        div.classList.add("active");
+      }
+      document.querySelector(".slider").appendChild(div);
+    }
+  }
+
+  let currentSlide = 0;
+  let autoSlide = 0;
+
+  function controlSlide(element) {
+    clearInterval(timer);
+    timer = setInterval(autoPlay, 5000);
+    autoSlide = element.id;
+    currentSlide = element.id;
+    changeSlide(currentSlide);
+  }
+
+  function changeSlide(currentSlide) {
+    const controlButtons = document.querySelector(".slider").children;
+    for (let i = 0; i < controlButtons.length; i++) {
+      controlButtons[i].classList.remove("active");
+    }
+    controlButtons[currentSlide].classList.add("active");
+
+    sliderContainer.style.marginLeft = -(containerWidth * currentSlide) + "px";
+  }
+
+  function autoPlay() {
+    if (autoSlide == sliderDot - 1) {
+      autoSlide = 0;
+    } else {
+      autoSlide++;
+    }
+    changeSlide(autoSlide);
+  }
+
+  let timer = setInterval(autoPlay, 5000);
+
+  window.onload = load();
+
+  // Get In Touch Button
+  const getInTouchButton = document.querySelector(".button.btn a");
+
+  if (getInTouchButton) {
+    getInTouchButton.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      // Get form values
+      const name = document.querySelector(".contact-form input[name='']").value;
+      const email = document.querySelector(".contact-form input[name='']").value;
+      const message = document.querySelector(".contact-form textarea").value;
+
+      // Validate form data
+      if (!name || !email || !message) {
+        alert("Please fill in all the fields.");
+        return;
+      }
+
+      // Prepare email content
+      const emailContent = "Name: " + name + "\n";
+      emailContent += "Email: " + email + "\n\n";
+      emailContent += "Message:\n" + message;
+
+      // Send email (you may replace this part with your preferred email sending method)
+      const mailTo = "gaikwadsp71@gmail.com";
+      const mailSubject = "New Contact Form Submission";
+      const mailBody = encodeURIComponent(emailContent);
+      const mailtoLink = "mailto:" + mailTo + "?subject=" + mailSubject + "&body=" + mailBody;
+
+      window.location.href = mailtoLink;
+    });
+  }
 });
-
-// Testimonials Slider
-
-const sliderContainer = document.querySelector(".testimonials-box");
-const slider = sliderContainer.children;
-const containerWidth = sliderContainer.offsetWidth;
-
-const margin = 30;
-let itemPerSlide = 0;
-let sliderDot;
-
-const responsive = [{
-    breakPoint: {
-      width: 0,
-      item: 1,
-    },
-  },
-  {
-    breakPoint: {
-      width: 991,
-      item: 2,
-    },
-  },
-];
-
-function load() {
-  for (let i = 0; i < responsive.length; i++) {
-    if (window.innerWidth > responsive[i].breakPoint.width) {
-      itemPerSlide = responsive[i].breakPoint.item;
-    }
-  }
-  start();
-}
-
-function start() {
-  totalWidth = 0;
-  for (let i = 0; i < slider.length; i++) {
-    slider[i].style.width = containerWidth / itemPerSlide - margin + "px";
-    slider[i].style.margin = margin / 2 + "px";
-    totalWidth += containerWidth / itemPerSlide;
-  }
-  sliderContainer.style.width = totalWidth + "px";
-
-  sliderDot = Math.ceil(slider.length / itemPerSlide);
-
-  for (let i = 0; i < sliderDot; i++) {
-    const div = document.createElement("div");
-    div.id = i;
-    div.setAttribute("onclick", "controlSlide(this)");
-    if (i == 0) {
-      div.classList.add("active");
-    }
-    document.querySelector(".slider").appendChild(div);
-  }
-}
-
-let currentSlide = 0;
-let autoSlide = 0;
-
-function controlSlide(element) {
-  clearInterval(timer);
-  timer = setInterval(autoPlay, 5000);
-  autoSlide = element.id;
-  currentSlide = element.id;
-  changeSlide(currentSlide);
-}
-
-function changeSlide(currentSlide) {
-  controlButton = document.querySelector(".slider").children;
-  for (let i = 0; i < controlButton.length; i++) {
-    controlButton[i].classList.remove("active");
-  }
-  controlButton[currentSlide].classList.add("active");
-
-  sliderContainer.style.marginLeft = -(containerWidth * currentSlide) + "px";
-}
-
-function autoPlay() {
-  if (autoSlide == sliderDot - 1) {
-    autoSlide = 0;
-  } else {
-    autoSlide++;
-  }
-  changeSlide(autoSlide);
-}
-let timer = setInterval(autoPlay, 5000);
-
-window.onload = load();
-
-
-//Footer Year 
-var year = document.getElementById("year");
-year.innerHTML = new Date().getFullYear();
